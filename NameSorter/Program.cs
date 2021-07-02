de2be5a226367e6c59d652d17e9733065e5756ab
rudhrakumar <rudhrakumar@gmail.com>
2021-07-03 09:27:58 +1000

    removed analyser nuget to fix build issue in appveyer

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -1,9 +1,7 @@
 ﻿using GlobalX.NameSorter;
 using GlobalX.NameSorter.Utils;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using GlobalX.NameSorter.Writer;
 using System.Reflection;
 using System.IO;
@@ -14,10 +12,12 @@ class Program
     static void Main(string[] args)
     {
         string inputFile = "";
+        //Find the executing assembly path
         string codeBase = Assembly.GetExecutingAssembly().CodeBase;
         UriBuilder uri = new UriBuilder(codeBase);
         string path = Uri.UnescapeDataString(uri.Path);
         var basePath = Path.GetDirectoryName(path);
+        // If there is no filename passed in the arguement use the default file
         if (args.Length == 0)
         {
             inputFile = $"{basePath}\\unsorted-names-list.txt";
@@ -26,7 +26,7 @@ class Program
         {
             inputFile = $"{basePath}\\{args[0]}";
         }
-        // read the file and sort the names
+        // Read the file and sort the names
         PersonFileReader fileReader = new PersonFileReader();
         List<Person> nameList =  fileReader.ReadUnSortedNameFile(inputFile);
         nameList.Sort(new PersonSortHelper());
